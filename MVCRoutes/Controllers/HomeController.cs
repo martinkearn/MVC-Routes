72ec25e145a0a91216656353df1605fb99cf2b8e
Martin Kearn <martin.kearn@microsoft.com>
2017-09-20 14:39:14 -0700

    Initial

---
+++ MVCRoutes/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCRoutes.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MVCRoutes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Codes()
+        {
+            var data = RouteData.Values["data"];
+            var dataValue = (data != null) ? RouteData.Values["data"].ToString() : string.Empty;
+
+
+            var vm = new CodesViewModel() { Data = dataValue };
+
+            return View(vm);
+        }
+
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVCRoutes/Models/CodesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCRoutes.Models
+{
+    public class CodesViewModel
+    {
+        public string Data { get; set; }
+    }
+}
